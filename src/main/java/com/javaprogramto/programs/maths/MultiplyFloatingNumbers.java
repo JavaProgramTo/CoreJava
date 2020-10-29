b00f2db3a118591576abf49f021d6ed19faed2e3
JavaProgramTo.com <admin@javaprogramto.com>
2020-10-29 20:15:37 +0530

    Java Program to Multiply Two Floating Point Numbers

---
+++ src/main/java/com/javaprogramto/programs/maths/MultiplyFloatingNumbers.java
@@ -0,0 +1,21 @@
+package com.javaprogramto.programs.maths;
+
+public class MultiplyFloatingNumbers {
+
+	public static void main(String[] args) {
+
+		// first floating number
+		float f1 = 2.5f;
+
+		// second floating number
+		float f2 = 4.0f;
+
+		// result of multiplication of two numbers.
+		float result = f1 * f2;
+
+		// printing
+		System.out.println("Result is : " + result);
+
+	}
+
+}
