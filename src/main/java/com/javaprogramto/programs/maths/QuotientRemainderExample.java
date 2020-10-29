8bd0c4a3bef79d347007b20be592b95ecf5e48b2
JavaProgramTo.com <admin@javaprogramto.com>
2020-10-29 18:55:09 +0530

    Java Program to Find Quotient and Remainder

---
+++ src/main/java/com/javaprogramto/programs/maths/QuotientRemainderExample.java
@@ -0,0 +1,24 @@
+package com.javaprogramto.programs.maths;
+
+public class QuotientRemainderExample {
+
+	public static void main(String[] args) {
+
+		// Creating int variable for number 1
+		int number1 = 200;
+
+		// Creating int variable for number 2
+		int number2 = 50;
+
+		// Calculating the quotient
+		int quotient = number1 / number2;
+		
+		// Calculating the remainder
+		int remainder = number1 % number2;
+		
+		System.out.println("Quotient value: "+quotient);
+		System.out.println("Remainder value with zero: "+remainder);
+
+	}
+
+}
