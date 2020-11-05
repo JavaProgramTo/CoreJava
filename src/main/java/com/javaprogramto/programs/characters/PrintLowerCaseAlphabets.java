073361a7b9543ddb636702be1917382fcf74aa68
JavaProgramTo.com <admin@javaprogramto.com>
2020-11-06 01:23:08 +0530

    Java Program To Print ALL Characters

---
+++ src/main/java/com/javaprogramto/programs/characters/PrintLowerCaseAlphabets.java
@@ -0,0 +1,21 @@
+package com.javaprogramto.programs.characters;
+
+public class PrintLowerCaseAlphabets {
+
+	public static void main(String[] args) {
+
+		// first character
+		char firstChar = 'a';
+
+		// last character
+		char lastChar = 'z';
+
+		// print lowercase letters from a to z
+		for (char c = firstChar; c <= lastChar; c++) {
+			System.out.print(c + " ");
+
+		}
+
+	}
+
+}
---
+++ src/main/java/com/javaprogramto/programs/characters/PrintUpperCaseAlphabets.java
@@ -0,0 +1,21 @@
+package com.javaprogramto.programs.characters;
+
+public class PrintUpperCaseAlphabets {
+
+	public static void main(String[] args) {
+
+		// first character
+		char firstChar = 'A';
+
+		// last character
+		char lastChar = 'Z';
+
+		// print uppercase letters from A to Z
+		for (char c = firstChar; c <= lastChar; c++) {
+			System.out.print(c + " ");
+
+		}
+
+	}
+
+}
